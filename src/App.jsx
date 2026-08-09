0a46d4509e89881949cdae355c32d0913603eaa5
ridu101 <ridu116540@gmail.com>
2026-08-09 19:39:26 +0600

    button togolling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
 
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import AvailablePlayers from "./Components/AvailablePlayers/AvailablePlayers";
-// import SelectedPlayers from "./Components/SelectedPlayers/SelectedPlayers";
+import SelectedPlayers from "./Components/SelectedPlayers/SelectedPlayers";
 import NewsLatter from "./Components/NewsLatter";
 import Footer from "./Components/Footer";
 
@@ -17,7 +17,7 @@ return res.json()
 }
 
 function App() {
-
+ const [toggle, setToggle] = useState()
   const playerPromise= fetchPlayers();
   return (
     <>
@@ -30,16 +30,25 @@ function App() {
     <Suspense>
       <Hero></Hero>
     </Suspense>
-    {/* Available Player */}
-    <Suspense fallback={<h2>Loading Data..........</h2>}>
+    {/* Toggling section */}
+    <div className=" max-w-300  mx-auto flex items-center justify-between mt-10">
+      <h1 className="text-xl font-bold">Available Players</h1>
+      <div>
+        <button onClick={()=> setToggle(true)} className={`py-3 px-4 border border-gray-400 rounded-l-2xl border-r-0  font-medium  ${ toggle === true? "bg-[#E7FE29]":"" }`} >Available</button>
+        <button onClick={()=> setToggle(false)} className={`py-3 px-4 border border-gray-400 rounded-r-2xl border-l-0  font-medium ${ toggle === false ? "bg-[#E7FE29]": ""}`}>Selected <span>(0)</span></button>
+      </div>
+    </div>
+    {
+      toggle === true ? <Suspense fallback={<h2>Loading Data..........</h2>}>
       <AvailablePlayers playerPromise={playerPromise}>
        
       </AvailablePlayers>
-    </Suspense>
-    {/* Selected Players */}
-    {/* <Suspense>
+    </Suspense> :  <Suspense>
       <SelectedPlayers></SelectedPlayers>
-    </Suspense> */}
+    </Suspense>
+    }
+    
+   
 
     {/* news latter */}
     <Suspense>
